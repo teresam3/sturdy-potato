1d65af0cd5f89b441371db4c5017448d926a1d39
chilimango <maldonadot55@yahoo.com>
2020-08-26 19:56:57 -0700

    connected api

--- reps/rep.script.js
+++ reps/rep.script.js
@@ -1,20 +1,21 @@
 $( document ).ready(function(){ 
     
-    var askUrl = 'https://www.googleapis.com/civicinfo/v2/elections?key=AIzaSyC-JifvUQEcdKveiVLyVEF_Oy_Ea2EmfGg'
+    var address = '14815 Dunton Dr'
+    var askUrl = 'https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyC-JifvUQEcdKveiVLyVEF_Oy_Ea2EmfGg&address=' + address
     
     $.ajax(
         askUrl
     ).then(function(data) {
         console.log(data)
     
-    var names = []  
+    // var names = []  
     
-        for (var i=0; i < data.elections.length; i++) {
-            names.push(data.elections[i].name)
-            // console.log(data.elections[i].name)
-            // $('.lead').append(data.elections[i].name)
-        } 
-        console.log(names)
+    //     // for (var i=0; i < data.elections.length; i++) {
+    //     //     names.push(data.elections[i].name)
+    //     //     // console.log(data.elections[i].name)
+    //     //     // $('.lead').append(data.elections[i].name)
+    //     // } 
+    //     // console.log(names)
     })
      
     
