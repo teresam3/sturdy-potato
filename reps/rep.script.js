48392bb13b0647dd0a900c5a6770da934bada498
chilimango <maldonadot55@yahoo.com>
2020-08-27 18:34:51 -0700

    rep

--- reps/rep.script.js
+++ reps/rep.script.js
@@ -1,21 +1,14 @@
 $( document ).ready(function(){ 
     
-    var address = '14815 Dunton Dr'
-    var askUrl = 'https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyC-JifvUQEcdKveiVLyVEF_Oy_Ea2EmfGg&address=' + address
+    let address = '14815 Dunton Dr'
+    Const askUrl = 'https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyC-JifvUQEcdKveiVLyVEF_Oy_Ea2EmfGg&address=' + address
     
     $.ajax(
         askUrl
     ).then(function(data) {
         console.log(data)
     
-    // var names = []  
-    
-    //     // for (var i=0; i < data.elections.length; i++) {
-    //     //     names.push(data.elections[i].name)
-    //     //     // console.log(data.elections[i].name)
-    //     //     // $('.lead').append(data.elections[i].name)
-    //     // } 
-    //     // console.log(names)
+
     })
      
     
