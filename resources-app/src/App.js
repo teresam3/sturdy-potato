9d6f423161096f423efb7c651c3e175f96d2144f
chilimango <maldonadot55@yahoo.com>
2021-01-05 15:49:03 -0800

    added boxes for page

--- resources-app/src/App.js
+++ resources-app/src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Jumbotron from "./components/Jumbotron";
+import Jumbotron from "./components/Jumbtron/Jumbotron";
 import Input from "./components/Input";
 
 function App() {
   return (
     <div>
-      <Navbar />;
-      <Jumbotron />;
-      <Input />;
+      <Navbar />
+      <Jumbotron />
+      <Input />
     </div>
   );
 }
--- resources-app/src/components/Jumbtron/Jumbotron.js
+++ resources-app/src/components/Jumbtron/Jumbotron.js
@@ -1,14 +1,15 @@
 import React from "react";
+import "./Jumbo.css";
 
 function Jumbotron() {
     return (
         <div>
-            <div class="container">
+            <div class="container-xl">
                 <h1 class="display-4">Fluid jumbotron</h1>
                 <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
             </div>
         </div>
-    );
+    )
 }
 
 export default Jumbotron;
